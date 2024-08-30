9cb3ec166415e7e0b3876c73ab0b2ea2d8f01930
Liam Brown <131519232+LiamB1017@users.noreply.github.com>
2024-08-30 23:42:45 +0200

    Add project files.

---
+++ ST10262451LiamBrownCLDV6212POE/Controllers/HomeController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ST10262451LiamBrownCLDV6212POE.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ST10262451LiamBrownCLDV6212POE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10262451LiamBrownCLDV6212POE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
