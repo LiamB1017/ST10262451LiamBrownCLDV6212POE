9c8e66fb75a40057ff1bd1ee682d9ad28388cd08
Liam Brown <131519232+LiamB1017@users.noreply.github.com>
2024-10-01 23:32:45 +0200

    Added ContractFile.cs

---
+++ ST10262451LiamBrownCLDV6212POE/Shared2/ContractFile.cs
@@ -0,0 +1,15 @@
+﻿// /Shared/ContractFile.cs
+using Azure.Storage.Files.Shares;
+using System;
+
+namespace ABC_Retail_App_Functions.Shared
+{
+    public class ContractFile
+    {
+        public string ShareName { get; set; } // e.g., "contracts"
+        public string DirectoryName { get; set; } // e.g., "legal"
+        public string FileName { get; set; }
+        public long FileSize { get; set; }
+        // Additional fields as needed
+    }
+}
