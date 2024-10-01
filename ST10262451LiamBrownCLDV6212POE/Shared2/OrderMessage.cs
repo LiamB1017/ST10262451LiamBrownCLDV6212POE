44b75ffd69039f59997111526f50b273ba23a258
Liam Brown <131519232+LiamB1017@users.noreply.github.com>
2024-10-01 23:31:48 +0200

    Added OrderMessage.cs

---
+++ ST10262451LiamBrownCLDV6212POE/Shared2/OrderMessage.cs
@@ -0,0 +1,11 @@
+﻿// /Shared/OrderMessage.cs
+namespace ABC_Retail_App_Functions.Shared
+{
+    public class OrderMessage
+    {
+        public string OrderId { get; set; }
+        public string ProductId { get; set; }
+        public int Quantity { get; set; }
+        // Additional fields as needed
+    }
+}
